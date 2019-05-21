8958b7a3f7ab6aa2d71fd98334e7bff4de9a5000
TP211 <1242410592@qq.com>
2019-05-21 19:32:47 +0800

    Add files via upload

---
+++ src/com/toy/anagrams/ui/About.java
@@ -0,0 +1,88 @@
+package com.toy.anagrams.ui;
+
+import java.awt.Dimension;
+import java.awt.Point;
+import java.awt.Rectangle;
+import java.awt.event.ActionListener;
+import javax.swing.JDialog;
+import javax.swing.JFrame;
+
+public class About extends JDialog {
+
+    public About(JFrame parent) {
+        super(parent,true);
+        initComponents();
+        pack();
+        Rectangle parentBounds = parent.getBounds();
+        Dimension size = getSize();
+        
+        int x = Math.max(0, parentBounds.x + (parentBounds.width - size.width) / 2);
+        int y = Math.max(0, parentBounds.y + (parentBounds.height - size.height) / 2);
+        setLocation(new Point(x, y));
+    }
+
+    // <editor-fold defaultstate="collapsed" desc="Generated Code">//GEN-BEGIN:initComponents
+    private void initComponents() {
+        java.awt.GridBagConstraints gridBagConstraints;
+
+        mainPanel = new javax.swing.JPanel();
+        copyrightTextArea = new javax.swing.JTextArea();
+        closeButton = new javax.swing.JButton();
+
+        setDefaultCloseOperation(javax.swing.WindowConstants.DISPOSE_ON_CLOSE);
+        setTitle("About Anagrams");
+        getContentPane().setLayout(new java.awt.GridBagLayout());
+
+        mainPanel.setBorder(javax.swing.BorderFactory.createEmptyBorder(11, 11, 12, 12));
+        mainPanel.setLayout(new java.awt.GridBagLayout());
+
+        copyrightTextArea.setEditable(false);
+        copyrightTextArea.setBackground(javax.swing.UIManager.getDefaults().getColor("Panel.background"));
+        copyrightTextArea.setColumns(25);
+        copyrightTextArea.setLineWrap(true);
+        copyrightTextArea.setRows(8);
+        copyrightTextArea.setText("单词排列游戏1.0a1中文版\n\n版权所有 (c) 1074-2015  TPLAPETA Inc.\n\nBy.TP211");
+        copyrightTextArea.setWrapStyleWord(true);
+        copyrightTextArea.setBorder(null);
+        copyrightTextArea.setFocusable(false);
+        gridBagConstraints = new java.awt.GridBagConstraints();
+        gridBagConstraints.fill = java.awt.GridBagConstraints.BOTH;
+        gridBagConstraints.anchor = java.awt.GridBagConstraints.NORTH;
+        gridBagConstraints.weightx = 1.0;
+        gridBagConstraints.weighty = 1.0;
+        gridBagConstraints.insets = new java.awt.Insets(24, 0, 24, 0);
+        mainPanel.add(copyrightTextArea, gridBagConstraints);
+
+        closeButton.setMnemonic('C');
+        closeButton.setText("关闭");
+        closeButton.addActionListener(new java.awt.event.ActionListener() {
+            public void actionPerformed(java.awt.event.ActionEvent evt) {
+                closeButtonActionPerformed(evt);
+            }
+        });
+        gridBagConstraints = new java.awt.GridBagConstraints();
+        gridBagConstraints.gridx = 0;
+        gridBagConstraints.gridy = 1;
+        gridBagConstraints.anchor = java.awt.GridBagConstraints.SOUTHEAST;
+        mainPanel.add(closeButton, gridBagConstraints);
+
+        gridBagConstraints = new java.awt.GridBagConstraints();
+        gridBagConstraints.fill = java.awt.GridBagConstraints.BOTH;
+        gridBagConstraints.weightx = 1.0;
+        gridBagConstraints.weighty = 1.0;
+        getContentPane().add(mainPanel, gridBagConstraints);
+    }// </editor-fold>//GEN-END:initComponents
+
+    private void closeButtonActionPerformed(java.awt.event.ActionEvent evt) {//GEN-FIRST:event_closeButtonActionPerformed
+        setVisible(false);
+        dispose();
+    }//GEN-LAST:event_closeButtonActionPerformed
+
+
+    // Variables declaration - do not modify//GEN-BEGIN:variables
+    private javax.swing.JButton closeButton;
+    private javax.swing.JTextArea copyrightTextArea;
+    private javax.swing.JPanel mainPanel;
+    // End of variables declaration//GEN-END:variables
+
+}
